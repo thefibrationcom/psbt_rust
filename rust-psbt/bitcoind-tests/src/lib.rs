b592a7979c2c7a77b760ba2339d9466b778458ba
thefibration <167394971+thefibration@users.noreply.github.com>
2024-04-22 14:14:27 +0500

    Delete directory

--- rust-psbt/bitcoind-tests/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Tools to help with testing against Bitcoin Core using [`bitcoind`] and [`bitcoincore-rpc`].
-//!
-//! [`bitcoind`]: <https://github.com/rust-bitcoin/bitcoind>
-//! [`bitcoincore-rpc`]: <https://github.com/rust-bitcoin/rust-bitcoincore-rpc/>
-
-/// A wrapper around the `bitcoind` client.
-pub mod client;
--- rust-psbt/bitcoind-tests/src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("\nTo run the tests use cargo test instead of cargo run.\n");
-}
